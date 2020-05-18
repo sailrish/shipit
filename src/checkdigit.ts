f43f5b4fea910bd838ab049f3fd8c57d34155bde
heuristicAL <giard.alexandre@gmail.com>
2020-05-18 01:11:07 -0400

    Fix checkDigit

--- src/checkdigit.ts
+++ src/checkdigit.ts
@@ -6,23 +6,23 @@
  * DS202: Simplify dynamic range loops
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-function _checkDigit (trk, multipliers, mod) {
-  let checkdigit
-  let midx = 0
-  let sum = 0
+function CheckDigit(trk: string, multipliers: number[], mod: number): boolean {
+  let checkdigit: number;
+  let midx = 0;
+  let sum = 0;
   for (let index = 0, end = trk.length - 2, asc = end >= 0; asc ? index <= end : index >= end; asc ? index++ : index--) {
-    sum += parseInt(trk[index], 10) * multipliers[midx]
-    midx = midx === (multipliers.length - 1) ? 0 : midx + 1
+    sum += parseInt(trk[index], 10) * multipliers[midx];
+    midx = midx === (multipliers.length - 1) ? 0 : midx + 1;
   }
   if (mod === 11) {
-    checkdigit = sum % 11
-    if (checkdigit === 10) { checkdigit = 0 }
+    checkdigit = sum % 11;
+    if (checkdigit === 10) { checkdigit = 0; }
   }
   if (mod === 10) {
-    checkdigit = 0
-    if ((sum % 10) > 0) { checkdigit = (10 - (sum % 10)) }
+    checkdigit = 0;
+    if ((sum % 10) > 0) { checkdigit = (10 - (sum % 10)); }
   }
-  return checkdigit === parseInt(trk[trk.length - 1])
+  return checkdigit === parseInt(trk[trk.length - 1]);
 }
 
-export default _checkDigit
+export { CheckDigit };
--- src/guessCarrier.ts
+++ src/guessCarrier.ts
@@ -10,6 +10,7 @@
  */
 import { upperCase } from 'change-case';
 import { uniq } from 'underscore';
+import { CheckDigit } from './checkdigit';
 
 function _preprocess (trk) {
   return upperCase(trk.replace(/\s+/g, ''));
@@ -35,61 +36,42 @@ function _confirmUps (trk) {
   return [false, false];
 }
 
-function _checkDigit (trk, multipliers, mod) {
-  let checkdigit;
-  let midx = 0;
-  let sum = 0;
-  for (let index = 0, end = trk.length - 2, asc = end >= 0; asc ? index <= end : index >= end; asc ? index++ : index--) {
-    sum += parseInt(trk[index], 10) * multipliers[midx];
-    midx = midx === (multipliers.length - 1) ? 0 : midx + 1;
-  }
-  if (mod === 11) {
-    checkdigit = sum % 11;
-    if (checkdigit === 10) { checkdigit = 0; }
-  }
-  if (mod === 10) {
-    checkdigit = 0;
-    if ((sum % 10) > 0) { checkdigit = (10 - (sum % 10)); }
-  }
-  return checkdigit === parseInt(trk[trk.length - 1]);
-}
-
 function _confirmUpsFreight (trk) {
   const firstChar = `${(trk.charCodeAt(0) - 63) % 10}`;
   const remaining = trk.slice(1);
   trk = `${firstChar}${remaining}`;
-  if (_checkDigit(trk, [3, 1, 7], 10)) { return [true, true]; }
+  if (CheckDigit(trk, [3, 1, 7], 10)) { return [true, true]; }
   return [false, false];
 }
 
 function _confirmFedex12 (trk) {
-  if (_checkDigit(trk, [3, 1, 7], 11)) { return [true, false]; }
+  if (CheckDigit(trk, [3, 1, 7], 11)) { return [true, false]; }
   return [false, false];
 }
 
 function _confirmFedexDoorTag (trk) {
-  if (_checkDigit(trk.match(/^DT(\d{12})$/)[1], [3, 1, 7], 11)) { return [true, true]; }
+  if (CheckDigit(trk.match(/^DT(\d{12})$/)[1], [3, 1, 7], 11)) { return [true, true]; }
   return [false, false];
 }
 
 function _confirmFedexSmartPost (trk) {
-  if (_checkDigit(`91${trk}`, [3, 1], 10)) {
+  if (CheckDigit(`91${trk}`, [3, 1], 10)) {
     return [true, false];
   }
   return [false, false];
 }
 
 function _confirmFedex15 (trk) {
-  if (_checkDigit(trk, [1, 3], 10)) { return [true, false]; }
+  if (CheckDigit(trk, [1, 3], 10)) { return [true, false]; }
   return [false, false];
 }
 
 function _confirmFedex20 (trk) {
-  if (_checkDigit(trk, [3, 1, 7], 11)) {
+  if (CheckDigit(trk, [3, 1, 7], 11)) {
     return [true, false];
   } else {
     const alteredTrk = `92${trk}`;
-    if (_checkDigit(alteredTrk, [3, 1], 10)) {
+    if (CheckDigit(alteredTrk, [3, 1], 10)) {
       return [true, false];
     }
   }
@@ -97,38 +79,38 @@ function _confirmFedex20 (trk) {
 }
 
 function _confirmUsps20 (trk) {
-  if (_checkDigit(trk, [3, 1], 10)) {
+  if (CheckDigit(trk, [3, 1], 10)) {
     return [true, false];
   }
   return [false, false];
 }
 
 function _confirmFedex9622 (trk) {
-  if (_checkDigit(trk, [3, 1, 7], 11)) { return [true, false]; }
-  if (_checkDigit(trk.slice(7), [1, 3], 10)) { return [true, false]; }
+  if (CheckDigit(trk, [3, 1, 7], 11)) { return [true, false]; }
+  if (CheckDigit(trk.slice(7), [1, 3], 10)) { return [true, false]; }
   return [false, false];
 }
 
 function _confirmUsps22 (trk) {
-  if (_checkDigit(trk, [3, 1], 10)) { return [true, false]; }
+  if (CheckDigit(trk, [3, 1], 10)) { return [true, false]; }
   return [false, false];
 }
 
 function _confirmUsps26 (trk) {
-  if (_checkDigit(trk, [3, 1], 10)) { return [true, false]; }
+  if (CheckDigit(trk, [3, 1], 10)) { return [true, false]; }
   return [false, false];
 }
 
 function _confirmUsps420Zip (trk) {
-  if (_checkDigit(trk.match(/^420\d{5}(\d{22})$/)[1], [3, 1], 10)) { return [true, false]; }
+  if (CheckDigit(trk.match(/^420\d{5}(\d{22})$/)[1], [3, 1], 10)) { return [true, false]; }
   return [false, false];
 }
 
 function _confirmUsps420ZipPlus4 (trk) {
-  if (_checkDigit(trk.match(/^420\d{9}(\d{22})$/)[1], [3, 1], 10)) {
+  if (CheckDigit(trk.match(/^420\d{9}(\d{22})$/)[1], [3, 1], 10)) {
     return [true, false];
   } else {
-    if (_checkDigit(trk.match(/^420\d{5}(\d{26})$/)[1], [3, 1], 10)) {
+    if (CheckDigit(trk.match(/^420\d{5}(\d{26})$/)[1], [3, 1], 10)) {
       return [true, false];
     }
   }
@@ -136,7 +118,7 @@ function _confirmUsps420ZipPlus4 (trk) {
 }
 
 function _confirmCanadaPost16 (trk) {
-  if (_checkDigit(trk, [3, 1], 10)) { return [true, false]; }
+  if (CheckDigit(trk, [3, 1], 10)) { return [true, false]; }
   return [false, false];
 }
 
