21916ce9634968d18be02a5648f5b6ca4a6d74a4
Alexandre Giard <Giard.Alexandre@gmail.com>
2020-05-20 00:10:24 -0400

    Cleanup a1

--- src/a1.ts
+++ src/a1.ts
@@ -11,22 +11,14 @@
 // Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
- * DS001: Remove Babel/TypeScript constructor workaround
- * DS101: Remove unnecessary use of Array.from
  * DS102: Remove unnecessary code created because of implicit returns
- * DS103: Rewrite code to no longer use __guard__
  * DS201: Simplify complex destructure assignments
- * DS206: Consider reworking classes to avoid initClass
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 import { Parser } from 'xml2js';
 import moment from 'moment-timezone';
 import { ShipperClient, STATUS_TYPES } from './shipper';
-
-function __guard__(value, transform) {
-  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
-}
 
 class A1Client extends ShipperClient {
   private STATUS_MAP = new Map<string, STATUS_TYPES>([
@@ -48,10 +40,10 @@ class A1Client extends ShipperClient {
   validateResponse(response, cb) {
     function handleResponse(xmlErr, trackResult) {
       if ((xmlErr != null) || (trackResult == null)) { return cb(xmlErr); }
-      const trackingInfo = __guard__(trackResult.AmazonTrackingResponse != null ? trackResult.AmazonTrackingResponse.PackageTrackingInfo : undefined, x => x[0]);
-      if ((trackingInfo != null ? trackingInfo.TrackingNumber : undefined) == null) {
-        const errorInfo = __guard__(trackResult.AmazonTrackingResponse != null ? trackResult.AmazonTrackingResponse.TrackingErrorInfo : undefined, x1 => x1[0]);
-        const error = __guard__(__guard__(__guard__(errorInfo != null ? errorInfo.TrackingErrorDetail : undefined, x4 => x4[0]), x3 => x3.ErrorDetailCodeDesc), x2 => x2[0]);
+      const trackingInfo = trackResult?.AmazonTrackingResponse?.PackageTrackingInfo?.[0];
+      if (trackingInfo?.TrackingNumber == null) {
+        const errorInfo = trackResult?.AmazonTrackingResponse?.TrackingErrorInfo?.[0];
+        const error = errorInfo?.TrackingErrorDetail?.[0]?.ErrorDetailCodeDesc?.[0];
         if (error != null) { return cb(error); }
         cb('unknown error');
       }
@@ -64,18 +56,18 @@ class A1Client extends ShipperClient {
 
   presentAddress(address) {
     if (address == null) { return; }
-    const city = address.City != null ? address.City[0] : undefined;
-    const stateCode = address.StateProvince != null ? address.StateProvince[0] : undefined;
-    const countryCode = address.CountryCode != null ? address.CountryCode[0] : undefined;
-    const postalCode = address.PostalCode != null ? address.PostalCode[0] : undefined;
+    const city = address?.City?.[0];
+    const stateCode = address?.StateProvince?.[0];
+    const countryCode = address?.CountryCode?.[0];
+    const postalCode = address?.PostalCode?.[0];
     return this.presentLocation({ city, stateCode, countryCode, postalCode });
   }
 
   getStatus(shipment) {
-    const lastActivity = __guard__(__guard__(shipment.TrackingEventHistory != null ? shipment.TrackingEventHistory[0] : undefined, x1 => x1.TrackingEventDetail), x => x[0]);
-    const statusCode = __guard__(lastActivity != null ? lastActivity.EventCode : undefined, x2 => x2[0]);
+    const lastActivity = shipment?.TrackingEventHistory?.[0]?.TrackingEventDetail?.[0];
+    const statusCode = lastActivity?.EventCode?.[0];
     if (statusCode == null) { return; }
-    const code = parseInt(__guard__(statusCode.match(/EVENT_(.*)$/), x3 => x3[1]));
+    const code = statusCode.match(/EVENT_(.*)$/)?.[1];
     if (isNaN(code)) { return; }
     if (this.STATUS_MAP.has(code.toString())) {
       return this.STATUS_MAP.get(code.toString());
@@ -90,33 +82,33 @@ class A1Client extends ShipperClient {
 
   getActivitiesAndStatus(shipment) {
     const activities = [];
-    const status = null;
-    let rawActivities: any[] = __guard__(shipment.TrackingEventHistory != null ? shipment.TrackingEventHistory[0] : undefined, x => x.TrackingEventDetail);
-    rawActivities = Array.from(rawActivities || []);
+    const status = this.getStatus(shipment);
+    let rawActivities: any[] = shipment?.TrackingEventHistory?.[0]?.TrackingEventDetail;
+    rawActivities = rawActivities ?? [];
     for (const rawActivity of rawActivities) {
       let datetime, timestamp;
-      const location = this.presentAddress(__guard__(rawActivity != null ? rawActivity.EventLocation : undefined, x1 => x1[0]));
-      const rawTimestamp = rawActivity != null ? rawActivity.EventDateTime[0] : undefined;
+      const location = this.presentAddress(rawActivity?.EventLocation?.[0]);
+      const rawTimestamp = rawActivity?.EventDateTime?.[0];
       if (rawTimestamp != null) {
         const eventTime = moment(rawTimestamp);
         timestamp = eventTime.toDate();
         datetime = rawTimestamp.slice(0, 19);
       }
-      const details = __guard__(rawActivity != null ? rawActivity.EventCodeDesc : undefined, x2 => x2[0]);
+      const details = rawActivity?.EventCodeDesc?.[0];
 
       if ((details != null) && (timestamp != null)) {
         const activity = { timestamp, datetime, location, details };
         activities.push(activity);
       }
     }
-    return { activities, status: this.getStatus(shipment) };
+    return { activities, status };
   }
 
   getEta(shipment) {
-    const activities = __guard__(shipment.TrackingEventHistory != null ? shipment.TrackingEventHistory[0] : undefined, x => x.TrackingEventDetail) || [];
+    const activities = shipment?.TrackingEventHistory?.[0]?.TrackingEventDetail || [];
     const firstActivity = activities[activities.length - 1];
-    if (__guard__(firstActivity != null ? firstActivity.EstimatedDeliveryDate : undefined, x1 => x1[0]) == null) { return; }
-    return moment(`${__guard__(firstActivity != null ? firstActivity.EstimatedDeliveryDate : undefined, x2 => x2[0])}T00:00:00Z`).toDate();
+    if (firstActivity?.EstimatedDeliveryDate?.[0] == null) { return; }
+    return moment(`${firstActivity?.EstimatedDeliveryDate?.[0]}T00:00:00Z`).toDate();
   }
 
   getService(shipment) {
@@ -128,7 +120,7 @@ class A1Client extends ShipperClient {
   }
 
   getDestination(shipment) {
-    return this.presentAddress(__guard__(shipment != null ? shipment.PackageDestinationLocation : undefined, x => x[0]));
+    return this.presentAddress(shipment?.PackageDestinationLocation?.[0]);
   }
 
   requestOptions({ trackingNumber }) {
