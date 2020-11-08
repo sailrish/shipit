08308ef22f4bc5b24d07036465de8545f0fbafcd
Alexandre Giard <giard.alexandre@gmail.com>
2020-11-08 13:33:11 -0500

    feat Format dhlgm.ts

--- src/dhlgm.ts
+++ src/dhlgm.ts
@@ -6,6 +6,7 @@
 	@typescript-eslint/no-unsafe-call,
 	node/no-callback-literal
 */
+import { upperCaseFirst } from "change-case";
 /* eslint-disable
     constructor-super,
     no-constant-condition,
@@ -26,29 +27,30 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-import { load } from 'cheerio';
-import moment from 'moment-timezone';
-import { upperCaseFirst } from 'change-case';
-import { ShipperClient, STATUS_TYPES } from './shipper';
+import { load } from "cheerio";
+import moment from "moment-timezone";
+import { ShipperClient, STATUS_TYPES } from "./shipper";
 
 function __guard__(value, transform) {
-  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
+  return typeof value !== "undefined" && value !== null
+    ? transform(value)
+    : undefined;
 }
 
 class DhlGmClient extends ShipperClient {
   private STATUS_MAP = new Map<string, STATUS_TYPES>([
-    ['electronic notification received', STATUS_TYPES.SHIPPING],
-    ['out for delivery', STATUS_TYPES.OUT_FOR_DELIVERY],
-    ['departure origin', STATUS_TYPES.EN_ROUTE],
-    ['transferred', STATUS_TYPES.EN_ROUTE],
-    ['cleared', STATUS_TYPES.EN_ROUTE],
-    ['received', STATUS_TYPES.EN_ROUTE],
-    ['processed', STATUS_TYPES.EN_ROUTE],
-    ['sorted', STATUS_TYPES.EN_ROUTE],
-    ['sorting complete', STATUS_TYPES.EN_ROUTE],
-    ['arrival', STATUS_TYPES.EN_ROUTE],
-    ['tendered', STATUS_TYPES.EN_ROUTE],
-    ['delivered', STATUS_TYPES.DELIVERED]
+    ["electronic notification received", STATUS_TYPES.SHIPPING],
+    ["out for delivery", STATUS_TYPES.OUT_FOR_DELIVERY],
+    ["departure origin", STATUS_TYPES.EN_ROUTE],
+    ["transferred", STATUS_TYPES.EN_ROUTE],
+    ["cleared", STATUS_TYPES.EN_ROUTE],
+    ["received", STATUS_TYPES.EN_ROUTE],
+    ["processed", STATUS_TYPES.EN_ROUTE],
+    ["sorted", STATUS_TYPES.EN_ROUTE],
+    ["sorting complete", STATUS_TYPES.EN_ROUTE],
+    ["arrival", STATUS_TYPES.EN_ROUTE],
+    ["tendered", STATUS_TYPES.EN_ROUTE],
+    ["delivered", STATUS_TYPES.DELIVERED],
   ]);
 
   constructor(options) {
@@ -58,7 +60,7 @@ class DhlGmClient extends ShipperClient {
 
   validateResponse(response, cb) {
     try {
-      response = response.replace(/<br>/gi, ' ');
+      response = response.replace(/<br>/gi, " ");
       return cb(null, load(response, { normalizeWhitespace: true }));
     } catch (error) {
       return cb(error);
@@ -66,49 +68,77 @@ class DhlGmClient extends ShipperClient {
   }
 
   extractSummaryField(data, name) {
-    if (data == null) { return; }
+    if (data == null) {
+      return;
+    }
     const $ = data;
     let value;
     const regex = new RegExp(name);
-    $('.card-info > dl').children().each(function (findex, field) {
-      if (regex.test($(field).text())) {
-        value = __guard__(__guard__($(field).next(), x1 => x1.text()), x => x.trim());
-      }
-      if (value != null) { return false; }
-    });
+    $(".card-info > dl")
+      .children()
+      .each(function (findex, field) {
+        if (regex.test($(field).text())) {
+          value = __guard__(
+            __guard__($(field).next(), (x1) => x1.text()),
+            (x) => x.trim()
+          );
+        }
+        if (value != null) {
+          return false;
+        }
+      });
     return value;
   }
 
   extractHeaderField(data, name) {
-    if (data == null) { return; }
+    if (data == null) {
+      return;
+    }
     const $ = data;
     let value;
     const regex = new RegExp(name);
-    $('.card > .row').children().each(function (findex, field) {
-      $(field).children().each((cindex, col) => $(col).find('dt').each(function (dindex, element) {
-        if (regex.test($(element).text())) {
-          return value = __guard__(__guard__($(element).next(), x1 => x1.text()), x => x.trim());
+    $(".card > .row")
+      .children()
+      .each(function (findex, field) {
+        $(field)
+          .children()
+          .each((cindex, col) =>
+            $(col)
+              .find("dt")
+              .each(function (dindex, element) {
+                if (regex.test($(element).text())) {
+                  return (value = __guard__(
+                    __guard__($(element).next(), (x1) => x1.text()),
+                    (x) => x.trim()
+                  ));
+                }
+              })
+          );
+        if (value != null) {
+          return false;
         }
-      }));
-      if (value != null) { return false; }
-    });
+      });
     return value;
   }
 
   getEta(data) {
-    if (data == null) { return; }
+    if (data == null) {
+      return;
+    }
     const $ = data;
-    const eta = $('.status-info > .row .est-delivery > p').text();
-    if (!(eta != null ? eta.length : undefined)) { return; }
+    const eta = $(".status-info > .row .est-delivery > p").text();
+    if (!(eta != null ? eta.length : undefined)) {
+      return;
+    }
     return moment(new Date(`${eta} 23:59:59 +00:00`)).toDate();
   }
 
   getService(data) {
-    return this.extractSummaryField(data, 'Service');
+    return this.extractSummaryField(data, "Service");
   }
 
   getWeight(data) {
-    return this.extractSummaryField(data, 'Weight');
+    return this.extractSummaryField(data, "Weight");
   }
 
   findStatusFromMap(statusText) {
@@ -131,26 +161,41 @@ class DhlGmClient extends ShipperClient {
   getActivitiesAndStatus(data) {
     let status = null;
     const activities = [];
-    if (data == null) { return { activities, status }; }
+    if (data == null) {
+      return { activities, status };
+    }
     const $ = data;
     let currentDate = null;
-    for (const rowData of Array.from($('.timeline').children() || [])) {
+    for (const rowData of Array.from($(".timeline").children() || [])) {
       const row = $(rowData);
-      if (row.hasClass('timeline-date')) { currentDate = row.text(); }
-      if (row.hasClass('timeline-event')) {
+      if (row.hasClass("timeline-date")) {
+        currentDate = row.text();
+      }
+      if (row.hasClass("timeline-event")) {
         let timestamp;
-        let currentTime = row.find('.timeline-time').text();
+        let currentTime = row.find(".timeline-time").text();
         if (currentTime != null ? currentTime.length : undefined) {
-          if (currentTime != null ? currentTime.length : undefined) { currentTime = __guard__(currentTime.trim().split(' '), x => x[0]); }
-          currentTime = currentTime.replace('AM', ' AM').replace('PM', ' PM');
-          timestamp = moment(new Date(`${currentDate} ${currentTime}`)).toDate();
+          if (currentTime != null ? currentTime.length : undefined) {
+            currentTime = __guard__(currentTime.trim().split(" "), (x) => x[0]);
+          }
+          currentTime = currentTime.replace("AM", " AM").replace("PM", " PM");
+          timestamp = moment(
+            new Date(`${currentDate} ${currentTime}`)
+          ).toDate();
         }
-        let location = row.find('.timeline-location-responsive').text();
+        let location = row.find(".timeline-location-responsive").text();
         location = location != null ? location.trim() : undefined;
-        if (location != null ? location.length : undefined) { location = upperCaseFirst(location); }
-        const details = __guard__(row.find('.timeline-description').text(), x1 => x1.trim());
-        if ((details != null) && (timestamp != null)) {
-          if (status == null) { status = this.presentStatus(details); }
+        if (location != null ? location.length : undefined) {
+          location = upperCaseFirst(location);
+        }
+        const details = __guard__(
+          row.find(".timeline-description").text(),
+          (x1) => x1.trim()
+        );
+        if (details != null && timestamp != null) {
+          if (status == null) {
+            status = this.presentStatus(details);
+          }
           activities.push({ details, location, timestamp });
         }
       }
@@ -159,13 +204,13 @@ class DhlGmClient extends ShipperClient {
   }
 
   getDestination(data) {
-    return this.extractHeaderField(data, 'To:');
+    return this.extractHeaderField(data, "To:");
   }
 
   requestOptions({ trackingNumber }) {
     return {
-      method: 'GET',
-      uri: `http://webtrack.dhlglobalmail.com/?trackingnumber=${trackingNumber}`
+      method: "GET",
+      uri: `http://webtrack.dhlglobalmail.com/?trackingnumber=${trackingNumber}`,
     };
   }
 }
