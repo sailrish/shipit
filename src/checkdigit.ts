4e34166de55a4539d083a102f4651fc1e0ebd7e0
Alexandre Giard <giard.alexandre@gmail.com>
2020-11-08 13:32:37 -0500

    feat Format dhl.ts and checkdigit.ts

--- src/checkdigit.ts
+++ src/checkdigit.ts
@@ -10,17 +10,25 @@ function CheckDigit(trk: string, multipliers: number[], mod: number): boolean {
   let checkdigit: number;
   let midx = 0;
   let sum = 0;
-  for (let index = 0, end = trk.length - 2, asc = end >= 0; asc ? index <= end : index >= end; asc ? index++ : index--) {
+  for (
+    let index = 0, end = trk.length - 2, asc = end >= 0;
+    asc ? index <= end : index >= end;
+    asc ? index++ : index--
+  ) {
     sum += parseInt(trk[index], 10) * multipliers[midx];
-    midx = midx === (multipliers.length - 1) ? 0 : midx + 1;
+    midx = midx === multipliers.length - 1 ? 0 : midx + 1;
   }
   if (mod === 11) {
     checkdigit = sum % 11;
-    if (checkdigit === 10) { checkdigit = 0; }
+    if (checkdigit === 10) {
+      checkdigit = 0;
+    }
   }
   if (mod === 10) {
     checkdigit = 0;
-    if ((sum % 10) > 0) { checkdigit = (10 - (sum % 10)); }
+    if (sum % 10 > 0) {
+      checkdigit = 10 - (sum % 10);
+    }
   }
   return checkdigit === parseInt(trk[trk.length - 1]);
 }
